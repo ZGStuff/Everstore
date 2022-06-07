5ce273bb36ecbfe099a2bfac1b955919ccdeda3e
ZGStuff <therealzapgames989@gmail.com>
2022-06-07 09:07:14 -0600

    edited stuf

--- everstorebase.go
+++ everstorebase.go
@@ -1,1 +1,9 @@
+package main
+package everbrew
 
+import fmt
+import everbrew
+
+func main() {
+	fmt Println("Hey! The Everstore isn't released yet. On release, this message will be removed and you can run this program using the Everbrew API and the Everbrew custom firmware.")
+}
